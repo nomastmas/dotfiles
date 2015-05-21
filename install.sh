e71db63eb56efbb14c0777e49f100e357181a5c4
Thomas Tsang <thomaswtsang@gmail.com>
2015-05-21 14:42:48 -0400

    add tmux symlink

--- install.sh
+++ install.sh
@@ -14,10 +14,13 @@ rm -f ~/.mybashrc
 rm -f ~/.bash_profile
 rm -f ~/.vimrc
 rm -rf ~/.vim
+rm -rf ~/.tmux.conf
 
 # Link config files in home folder
 ln -sf $PWD/alias.sh ~/.alias
 ln -sf $PWD/bash_profile.bash ~/.bash_profile
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/vim ~/.vim
+ln -sf $PWD/tmux_conf.sh ~/.tmux.conf
+
 #ln -sf ~/dotfiles/sh/git-completion.sh ~/.git-completion.sh
