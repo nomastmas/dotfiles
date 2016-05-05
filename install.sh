043e0bb7ba211076b58cd057ae6e4483c4398ddb
Thomas Tsang <thomaswtsang@gmail.com>
2016-05-05 11:17:53 -0400

    add git completion

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Remove config file links in home folder
 rm -f ~/.alias
 rm -f ~/.mybashrc
+rm -f ~/.git_completion.bash
 rm -f ~/.bash_profile
 rm -f ~/.vimrc
 rm -rf ~/.vim
@@ -14,6 +15,7 @@ ln -sf $PWD/bash_profile.bash ~/.bash_profile
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/vim ~/.vim
 ln -sf $PWD/tmux_conf.sh ~/.tmux.conf
+ln -sf $PWD/git_completion.bash ~/.git_completion.bash
 
 # Setup Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
