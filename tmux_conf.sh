5a3065a4576a0b7635c5e4fb1680ebb18b19daf3
Thomas Tsang <thomaswtsang@gmail.com>
2016-11-07 23:56:21 -0500

    bind C-a to prefix; resize with keys

--- tmux_conf.sh
+++ tmux_conf.sh
@@ -5,6 +5,9 @@ bind r source-file ~/.tmux.conf
 set -g set-titles on
 set -g set-titles-string '#(whoami)::#h::#(curl ipecho.net/plain;echo)'
 
+# Set base index
+set-window-option -g pane-base-index 1
+
 # Status bar customization
 set -g status-utf8 on
 set -g status-bg black
@@ -28,8 +31,27 @@ bind-key h select-pane -L
 unbind-key l
 bind-key l select-pane -R
 
+# Resize pane
+bind-key J resize-pane -D 5
+bind-key K resize-pane -U 5
+bind-key H resize-pane -L 5
+bind-key L resize-pane -R 5
+
+# Split pane
+bind-key v split-window -h
+bind-key s split-window -v
+
+# Remap prefix to C-a
+unbind C-b
+set -g prefix C-a
+# bind 'C-a C-a' to type 'C-a'
+bind C-a send-prefix
+
 # for syntax coloring in vim
 set -g default-terminal "xterm"
 
 # update environment for ssh
 set -g update-environment -r
+
+# Reload tmux config
+bind r source-file ~/.tmux.conf
